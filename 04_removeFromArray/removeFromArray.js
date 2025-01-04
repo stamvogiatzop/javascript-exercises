f3979bb43ed2674c6b3bbc6d622016ac1030487c
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-04 18:10:47 +0100

    finish with a little help from my friends... the solution file.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const filteredArray = []
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      filteredArray.push(item);
+    }
+  });
+  return filteredArray;
 };
 
 // Do not edit below this line
