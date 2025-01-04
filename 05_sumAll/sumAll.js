91219f830aee3f893c956d176ab3e631723a1260
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-04 19:17:19 +0100

    that was a doozy... careful with if parameters and remember the += addition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let finalSum = 0;
+
+  if (((num1 < 0) || (num2 < 0)) ||
+    ((num1 % 1 !== 0) && (num2 % 1 !== 0)) ||
+    ((!Number.isInteger(num1)) || (!Number.isInteger(num2)))) {
+      return "ERROR";
+    }
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+
+  while (num1 <= num2 ) {
+    finalSum += num1;
+    num1++;
+  }
+  return finalSum;
 
 };
 
