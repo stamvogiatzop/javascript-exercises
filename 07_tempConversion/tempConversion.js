16c05b160ec3d60c579e8c2dd627379e654a243e
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-05 17:41:17 +0100

    solved it with toFixed instead of Math.round. Need to check what is the difference though.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(grade) {
+  let celsiusGrade = parseFloat(((grade - 32) * (5 / 9)).toFixed(1));
+  return celsiusGrade;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(grade) {
+  let fahrenheitGrade = parseFloat((grade * (9 / 5) + 32).toFixed(1));
+  return fahrenheitGrade;
 };
 
 // Do not edit below this line
