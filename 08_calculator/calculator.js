47d91f97524059a800f54e6c7593d4b2f6a585e4
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-05 18:03:45 +0100

    start with calculator, finished addition and subtraction, working on array sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  if (num1 >= 0 && num2 >= 0) {
+    let sum = num1 + num2;
+    return sum;
+  } else {
+    return "only positive numbers";
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let remainder = num1 - num2;
+  return remainder;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arraySum = array;
+  return arraySum;
 };
 
 const multiply = function() {
@@ -15,11 +22,11 @@ const multiply = function() {
 };
 
 const power = function() {
-	
+
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
